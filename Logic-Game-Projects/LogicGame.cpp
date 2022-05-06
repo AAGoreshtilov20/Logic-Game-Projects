bd7c2ced318f70d706a93fc74c046e0811010cee
Yanislav Yanev <85335574+YIYanev20@users.noreply.github.com>
2022-05-06 11:47:27 +0300

    Added structures + generation remake (1/2)

--- Logic-Game-Projects/LogicGame.cpp
+++ Logic-Game-Projects/LogicGame.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //Legend:
-//r = row
+//r = row of cards; row = row of chars/strings
 //gen = generation, generating
 //0th row = the five preset bools for each player
 //[x][y][z] = [player][row][card] respectfully !!!only when context = pl variable!!!; in normal for-s x, y, z are used as normal dimensions
@@ -13,107 +13,166 @@ using namespace std;
 //UI: a = and, o = or, x = xor, n = not
 //Meaning of values in pl: and 0 = 10, and 1 = 11, or 0 = 20, or 1 = 21, xor 0 = 30, xor 1 = 31
 
-int pl[2][5][5]; //player var [player(1 or 2)][row][card]; not all cards are going to be used!
-int pos; //the next to be placed card's position
-bool val; //value for card
 char nothing;
-string card; //name of card (and, or, xor)
+int pl; //whoose player's turn is
+int rNum; //current r's/row's number
 
-void empty()
+struct card
 {
-    cout << "        "; //5 spaces margin on left side, 3 for missing card
+    string name; //and, or, xor
+    bool val; //value for card
+    string row[6] = { " _____ ", "|^   ^|", "", "", "|  |  |", "|_____|" };
+}
+def, fd[2][5][5], hand[2][6]; //default var; field var [player(1 or 2)][row][card], hand var [player(1 or 2)][card]
+
+string empty(bool b) 
+{
+    if (b)
+    {
+        return "       "; //7 spaces (replaces a card)
+    }
+    else
+    {
+        return "     "; //5 spaces (space between cards)
+    }
 }
 
-void r(int p, int rNum)
+void rowGen3(int cNum) 
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cout << empty(0);
+        if (fd[pl][rNum][i].val != NULL)
+        {
+            cout << "|  " << fd[pl][rNum][i].val << "  |";
+        }
+        else
+        {
+            cout << "  #" << i + cNum << "   ";
+        }
+    }
+}
+void rowGen2()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cout << empty(0);
+        if (fd[pl][rNum][i].val != NULL)
+        {
+            cout << "| " << fd[pl][rNum][i].name << " |";
+        }
+        else
+        {
+            cout << empty(1);
+        }
+    }
+}
+void rowGen1(int rowS) //rowS = row Specific; cNum = empty card's UI number
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cout << empty(0);
+        if (fd[pl][rNum][i].val != NULL)
+        {
+            cout << def.row[rowS];
+        }
+        else
+        {
+            cout << empty(1);
+        }
+    }
+    cout << endl;
+}
+void rGen()
 {
     switch (rNum)
     {
     case 0:
         for (int z = 0; z < 5; z++)
         {
-            cout << "      |" << pl[p][0][z] << "|"; //5 spaces infront
+            cout << empty(0) << def.row[0];
         }
+        cout << endl;
+
+        for (int z = 0; z < 5; z++)
+        {
+            cout << empty(0) << "|  " << fd[pl][rNum][z].val << "  |"; //5 spaces infront
+        }
+        cout << endl;
+
+        for (int z = 0; z < 5; z++)
+        {
+            cout << empty(0) << def.row[4];
+        }
+        cout << endl;
+
+        for (int z = 0; z < 5; z++)
+        {
+            cout << empty(0) << def.row[5];
+        }
+        cout << endl << endl;
         break;
+
     case 1:
-        cout << setw(10);
-        for (int z = 0; z < 4; z++)
-        {
-            cout << "     ";
-            if (pl[p][1][z] != 0)
-            {
-                cout << "|" << pl[p][0][z] << "|";
-            }
-            else
-            {
-                cout << " #" << z + 1 << " ";
-            }
-        }
+        rowGen1(0);
+        rowGen1(1);
+        rowGen2();
+        rowGen3(1);
+        rowGen1(4);
+        rowGen1(5);
+        cout << endl;
         break;
+
     case 2:
-        cout << setw(14);
-        for (int z = 0; z < 3; z++)
-        {
-            cout << "     ";
-            if (pl[p][2][z] != 0)
-            {
-                cout << "|" << pl[p][0][z] << z << "|";
-            }
-            else
-            {
-                cout << " #" << z + 5 << " ";
-            }
-        }
+        rowGen1(0);
+        rowGen1(1);
+        rowGen2();
+        rowGen3(5);
+        rowGen1(4);
+        rowGen1(5);
+        cout << endl;
         break;
+
     case 3:
-        cout << setw(18);
-        for (int z = 0; z < 2; z++)
-        {
-            cout << "     ";
-            if (pl[p][3][z] != 0)
-            {
-                cout << "|" << pl[p][0][z] << "|";
-            }
-            else
-            {
-                cout << " #" << z + 8 << " ";
-            }
-        }
+        rowGen1(0);
+        rowGen1(1);
+        rowGen2();
+        rowGen3(8);
+        rowGen1(4);
+        rowGen1(5);
+        cout << endl;
         break;
+
     case 4:
-        cout << setw(26);
-        if (pl[p][4][0] != 0)
-        {
-            cout << "|" << pl[p][0][0] << "|";
-        }
-        else
-        {
-            cout << " #10";
-        }
+        rowGen1(0);
+        rowGen1(1);
+        rowGen2();
+        rowGen3(10);
+        rowGen1(4);
+        rowGen1(5);
+        cout << endl;
         break;
     }
     cout << endl;
 }
-void fieldGen(int p)
+void fieldGen()
 {
-    for (int x = 0; x < 5; x++)
+    for (int i = 0; i < 5; i++)
     {
-        r(p, x);
+        rGen();
+        rNum = i;
     }
 }
 
-void turn(int p)
+void turn()
 {
     int y, z;
+    int pos;
 
-    fieldGen(p); //gen visuals
-    cout << "\n\n Which card do you want to place? ";
-    cin >> card;
-    cout << " What is its value? ";
-    cin >> val;
-    cout << " Where do you want to place it? ";
+    fieldGen(); //gen visuals
+
+    cout << "\n\n Where do you want to place your card? ";
     cin >> pos;
-    system("CLS"); //cout << card << val << pos << endl; <- for testing
-
     switch (pos)
     {
     case 1:
@@ -157,86 +216,43 @@ void turn(int p)
         z = 0;
         break;
     }
-    
-    if (card == "and")
-    {
-        if (val == 0)
-        {
-            pl[p][y][z] = 10;
-        }
-        else
-        {
-            pl[p][y][z] = 11;
-        }
-    }
-    else if (card == "or")
-    {
-        if (val == 0)
-        {
-            pl[p][y][z] = 20;
-        }
-        else
-        {
-            pl[p][y][z] = 21;
-        }
-    }
-    else if (card == "xor")
-    {
-        if (val == 0)
-        {
-            pl[p][y][z] = 30;
-        }
-        else
-        {
-            pl[p][y][z] = 31;
-        }
-    }
 
-    fieldGen(p);
+    cout << " Which card do you want to place? ";
+    cin >> fd[pl][y][z].name;
+    cout << " What is its value? ";
+    cin >> fd[pl][y][z].val;
+    if (fd[pl][y][z].name == "or")
+    {
+        fd[pl][y][z].name = "or ";
+    }
+    system("CLS");
+
+    fieldGen();
     cin >> nothing;
     system("CLS"); //end of turn
 }
 
 int draw()
 {
-    srand((unsigned)time(NULL));
-
-   
-    for (int x = 1; x <= 1; x++) // Loop to get 1 or more random numbers
-    {
- 
-        int random = 1 + (rand() % 6); // Retrieve a random number between 1 and 5
-    }
-
-    return random; //Returns the random number
-
+    return 1 + (rand() % 6); // Retrieve a random number between 1 and 6
 }
 
 int main()
 {
     srand(time(NULL)); //call srand once to get random seed from time
 
-    for (int x = 0; x < 2; x++) //gen 0th row nums
+    for (int z = 0; z < 5; z++)
     {
-        for (int z = 0; z < 5; z++)
-        {
-            pl[x][0][z] = rand() % 2; //cout << x << 0 << z << pl[x][0][z] << endl; <- for testing   
-        }
-    }
-    for (int x = 0; x < 2; x++) //gen empty rows and cards nums
-    {
-        for (int y = 1; y < 5; y++) //y = 1 cuz not counting 0th row
-        {
-            for (int z = 0; z < 5; z++)
-            {
-                pl[x][y][z] = 0;
-            }
-        }
+        fd[0][0][z].val = rand() % 2;
+        fd[1][0][z].val = !fd[0][0][z].val;
     }
 
-    while(true) //start of game
+    while (true) //start of game
     {
-        turn(0); //start of player1's turn
-        turn(1); //start of player2's turn        
+        pl = 1;
+        turn(); //start of player1's turn
+
+        pl = 2;
+        turn(); //start of player2's turn
     }
 }
