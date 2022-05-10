c478fb66470448829fb822dffeaef0cb78562f11
Andrey Mutlu <AAMutlu20@codingburgas.bg>
2022-05-10 22:53:10 +0300

    Added SFML to project

--- Logic-Game-Projects/LogicGame.cpp
+++ Logic-Game-Projects/LogicGame.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include "SFML/Graphics.hpp"
 
 using namespace std;
 using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(600, 600), "BuuLeoGame.exe", Style::Default);
-	Event e;
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(e))
-		{
-			switch (e.type) // closing window
-			{
-				case Event::Closed:
-					window.close();
-
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Key::Escape) == true)// closing window
-			{
-				window.close();
-			}
-
-
-			if (e.type == Event::Closed)
-				window.close();
-		}
-	}
-	//mouse::isButtonPressed;
-
 	return 0;
 }
